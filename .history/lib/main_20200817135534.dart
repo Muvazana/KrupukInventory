86cfd06cd3bb031825f16b84b79cd4c0e63692ab
Muvazana-stdy <muvazana@gmail.com>
2020-08-17 14:03:34 +0700

    update assets

--- .history/lib/main_20200817135534.dart
+++ .history/lib/main_20200817135534.dart
@@ -13,6 +13,9 @@ class MyApp extends StatelessWidget {
       title: 'My Inventory',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: ,
